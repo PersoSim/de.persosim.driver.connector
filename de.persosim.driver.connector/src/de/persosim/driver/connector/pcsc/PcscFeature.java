4ca82e6f23e8b14d0fd9bcb96d4a43111c50744c
Martin Boonk <martin.boonk@hjp-consulting.com>
2014-08-25 12:06:31 +0200

    Combined with the PcscListener interface

--- de.persosim.driver.connector/src/de/persosim/driver/connector/pcsc/PcscFeature.java
+++ de.persosim.driver.connector/src/de/persosim/driver/connector/pcsc/PcscFeature.java
@@ -8,19 +8,7 @@ import de.persosim.simulator.tlv.TlvDataObject;
  * @author mboonk
  * 
  */
-public interface PcscFeature {
-	/**
-	 * Implementations should use this method to process the data received from
-	 * the native PCSC system.
-	 * 
-	 * @param controlCode
-	 *            the functions control code
-	 * @param data
-	 *            the input data
-	 * @return the result of this features calculations as byte array
-	 */
-	public abstract byte[] process(byte[] controlCode, byte[] data);
-
+public interface PcscFeature extends PcscListener {
 	/**
 	 * @return the capabilities provided by this PCSC feature
 	 */
