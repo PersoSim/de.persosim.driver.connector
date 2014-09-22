c8b89ceccf2d069e0e33b43d29c28f97bfc12d81
Martin Boonk <martin.boonk@hjp-consulting.com>
2014-09-22 16:11:45 +0200

    Added missing file for previous commit

--- de.persosim.driver.connector.ui/src/de/persosim/driver/connector/ui/NativeDriverConnectorConsole.java
+++ de.persosim.driver.connector.ui/src/de/persosim/driver/connector/ui/NativeDriverConnectorConsole.java
@@ -5,6 +5,7 @@ import java.net.UnknownHostException;
 
 import de.persosim.driver.connector.NativeDriverConnector;
 import de.persosim.driver.connector.PcscPrinter;
+import de.persosim.driver.connector.UnsignedInteger;
 import de.persosim.driver.connector.protocols.pace.PersoSimPcscProcessor;
 
 /**
@@ -22,7 +23,7 @@ public class NativeDriverConnectorConsole {
 				"localhost", 5678, "localhost", 9876);
 		connector.addUi(new ConsoleUi());
 		connector.addListener(new PcscPrinter());
-		connector.addListener(new PersoSimPcscProcessor(5));
+		connector.addListener(new PersoSimPcscProcessor(new UnsignedInteger(5)));
 		connector.connect();
 
 	}
