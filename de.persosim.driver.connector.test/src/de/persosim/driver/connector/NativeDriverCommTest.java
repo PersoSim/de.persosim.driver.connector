9efe080fe1920472c9b10de21dc2c99a57ed9d24
Martin Boonk <martin.boonk@hjp-consulting.com>
2014-10-08 14:43:27 +0200

    Additional test for the NativeDriverComm class

--- de.persosim.driver.connector.test/src/de/persosim/driver/connector/NativeDriverCommTest.java
+++ de.persosim.driver.connector.test/src/de/persosim/driver/connector/NativeDriverCommTest.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import de.persosim.driver.connector.pcsc.PcscCallData;
 import de.persosim.driver.connector.pcsc.PcscConstants;
 import de.persosim.driver.connector.pcsc.PcscListener;
+import de.persosim.driver.connector.pcsc.SimplePcscCallResult;
 import de.persosim.driver.test.ConnectorTest;
 import de.persosim.driver.test.TestDriver;
 
@@ -43,6 +44,11 @@ public class NativeDriverCommTest extends ConnectorTest{
 		driver.stop();
 	}
 
+	/**
+	 * Positive test using valid PCSC data.
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	@Test
 	public void testThreadValidPcsc() throws IOException, InterruptedException{
 //		prepare the mock
@@ -50,7 +56,7 @@ public class NativeDriverCommTest extends ConnectorTest{
 		listeners.add(mockedListener);
 		new Expectations() {{
 			mockedListener.processPcscCall((PcscCallData) any);
-			result = null;
+			result = new SimplePcscCallResult(PcscConstants.IFD_SUCCESS);
 		}};
 		
 		nativeComm.start();
@@ -60,6 +66,34 @@ public class NativeDriverCommTest extends ConnectorTest{
 		}
 		
 		String data = driver.sendData(new UnsignedInteger(0), new UnsignedInteger(0), new byte [0]);
+		assertEquals(PcscConstants.IFD_SUCCESS, UnsignedInteger.parseUnsignedInteger(data.split(Pattern.quote(NativeDriverInterface.MESSAGE_DIVIDER))[0], 16));
+	}
+
+	/**
+	 * Negative test using invalid data. An IFD_NOT_SUPPORTED is expected and
+	 * the listeners process method should not be called.
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testThreadInvalidPcsc() throws IOException, InterruptedException{
+//		prepare the mock
+
+		listeners.add(mockedListener);
+		new Expectations() {{
+			mockedListener.processPcscCall((PcscCallData) any);
+			times = 0;
+			result = new SimplePcscCallResult(PcscConstants.IFD_SUCCESS);
+		}};
+		
+		nativeComm.start();
+		
+		while (!nativeComm.isConnected()){
+			Thread.sleep(5);
+		}
+		
+		String data = driver.sendDataDirect(new UnsignedInteger(0), "j09uc540q93rufq09u,³48	q3m05ru");
 		assertEquals(PcscConstants.IFD_NOT_SUPPORTED, UnsignedInteger.parseUnsignedInteger(data.split(Pattern.quote(NativeDriverInterface.MESSAGE_DIVIDER))[0], 16));
 	}
 }
