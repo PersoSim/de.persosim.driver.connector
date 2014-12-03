7f54a03fa265b191625aa5d2c8d793ccaf2a4489
Martin Boonk <martin.boonk@hjp-consulting.com>
2014-12-03 14:55:05 +0100

    Additional unit tests for the fake streams

--- de.persosim.driver.connector.test/src/de/persosim/driver/test/streams/FakeInputStreamTest.java
+++ de.persosim.driver.connector.test/src/de/persosim/driver/test/streams/FakeInputStreamTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,10 +14,17 @@ import de.persosim.driver.test.ConnectorTest;
 public class FakeInputStreamTest extends ConnectorTest {
 
 	byte[] testData;
+	InputStream stream;
 
 	@Before
 	public void setUp() {
 		testData = new byte[] { 1, 2, 3, 4, 5 };
+		stream = new FakeInputStream(testData);
+	}
+	
+	@After
+	public void tearDown() throws IOException{
+		stream.close();
 	}
 
 	/**
@@ -25,11 +33,21 @@ public class FakeInputStreamTest extends ConnectorTest {
 	 */
 	@Test
 	public void testReadSimple() throws IOException {
-		InputStream stream = new FakeInputStream(testData);
 		for (int i = 0; i < testData.length; i++){
 			assertEquals(testData[i], stream.read());
 		}
 		assertEquals(-1, stream.read());
-		stream.close();
+	}
+
+	/**
+	 * Positive test case: read the stream all at once
+	 * @throws IOException
+	 */
+	@Test
+	public void testReadComplete() throws IOException{
+		byte [] received = new byte [testData.length];
+		int readNumber = stream.read(received);
+		assertEquals(testData.length, readNumber);
+		assertArrayEquals(testData, received);
 	}
 }
