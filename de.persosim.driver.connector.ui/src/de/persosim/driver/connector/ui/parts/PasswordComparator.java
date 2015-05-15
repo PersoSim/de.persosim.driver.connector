d80b6a9fb5c883d5626dff33fb8c65c2d4f783cd
Lukas Stauffenberg <lukas.stauffenberg@hjp-consulting.com>
2015-05-15 12:57:10 +0200

    The PasswordComparator parses the list to long to make it possible to have a password with the length of 11 signs. The sorting is now assurgent.

--- de.persosim.driver.connector.ui/src/de/persosim/driver/connector/ui/parts/PasswordComparator.java
+++ de.persosim.driver.connector.ui/src/de/persosim/driver/connector/ui/parts/PasswordComparator.java
@@ -4,17 +4,17 @@ import java.util.Comparator;
 
 /**
  * The PasswordComparator can be used for sorting string lists.
- * The sorted lists are beeing parsed to int, so they can be sorted degressive.
+ * The sorted lists are beeing parsed to long, so they can be sorted assurgent.
  * 
  * @param A String list
- * @return a degressive sorted list
+ * @return a assurgent sorted list
  */
 public class PasswordComparator implements Comparator<String> {
 
 	@Override
 	public int compare(String password1, String password2) {
 		
-		return Integer.parseUnsignedInt(password2) - Integer.parseUnsignedInt(password1);
+		return (int) Math.signum(Long.parseLong(password1) - Long.parseLong(password2));
 		
 		
 	}
