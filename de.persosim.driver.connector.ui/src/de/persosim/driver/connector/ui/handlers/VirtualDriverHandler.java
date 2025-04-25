8b9f86f8ecbc9cbe16079d1c53537cee1bac9f17
Martin Boonk <martin.boonk@secunet.com>
2025-04-25 18:35:10 +0200

    PS-11 - Set initial button states and sync internal state

--- de.persosim.driver.connector.ui/src/de/persosim/driver/connector/ui/handlers/VirtualDriverHandler.java
+++ de.persosim.driver.connector.ui/src/de/persosim/driver/connector/ui/handlers/VirtualDriverHandler.java
@@ -1,12 +1,10 @@
  
 package de.persosim.driver.connector.ui.handlers;
 
-import jakarta.inject.Inject;
-
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.menu.MItem;
-import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.globaltester.logging.BasicLogger;
@@ -17,28 +15,28 @@ import de.persosim.driver.connector.exceptions.IfdCreationException;
 import de.persosim.driver.connector.ui.parts.ReaderPart;
 
 public class VirtualDriverHandler {
-
-	@Inject
-	private EPartService partService;
-
+	
+	@CanExecute
+	public boolean checkState(final MPart mPart, final MItem mItem) {
+		if (mPart.getObject() instanceof ReaderPart) {
+			ReaderPart readerPartObject = (ReaderPart) mPart.getObject();
+			mItem.setSelected(readerPartObject.getCurrentCommType() == VirtualDriverComm.NAME);
+		}
+		return true;
+	}
+	
 	@Execute
 	public void execute(final MPart mPart, final MItem mItem) {
-		if (mItem.isSelected()) {
-			BasicLogger.log(this.getClass(), "Switch to use virtual driver", LogLevel.INFO);
+		BasicLogger.log(this.getClass(), "Switch to use virtual driver", LogLevel.INFO);
+		
+		if (mPart.getObject() instanceof ReaderPart) {
+			ReaderPart readerPartObject = (ReaderPart) mPart.getObject();
 
-			// ID of part as defined in fragment.e4xmi application model
-			MPart readerPart = partService.findPart("de.persosim.driver.connector.ui.parts.reader");
-
-			
-			if (readerPart.getObject() instanceof ReaderPart) {
-				ReaderPart readerPartObject = (ReaderPart) readerPart.getObject();
-
-				try {
-					readerPartObject.switchReaderType(new VirtualDriverComm(VirtualDriverComm.DEFAULT_HOST, VirtualDriverComm.DEFAULT_PORT));
-				} catch (IfdCreationException e) {
-					BasicLogger.logException(this.getClass(), e, LogLevel.WARN);
-					MessageDialog.openWarning(Display.getCurrent().getActiveShell(), "Connector could not be created", "The virtual driver communication could not be established.");
-				}
+			try {
+				readerPartObject.switchReaderType(new VirtualDriverComm(VirtualDriverComm.DEFAULT_HOST, VirtualDriverComm.DEFAULT_PORT));
+			} catch (IfdCreationException e) {
+				BasicLogger.logException(this.getClass(), e, LogLevel.WARN);
+				MessageDialog.openWarning(Display.getCurrent().getActiveShell(), "Connector could not be created", "The virtual driver communication could not be established.");
 			}
 		}
 	}
--- de.persosim.driver.connector/src/de/persosim/driver/connector/CommManager.java
+++ de.persosim.driver.connector/src/de/persosim/driver/connector/CommManager.java
@@ -56,6 +56,6 @@ public class CommManager {
 				}
 			}
 		}
-		return null;
+		return candidate;
 	}
 }
