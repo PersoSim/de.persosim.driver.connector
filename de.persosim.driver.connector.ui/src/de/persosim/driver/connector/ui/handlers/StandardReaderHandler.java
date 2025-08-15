993ac0d9a318bf6ff1db71cd90119c3800c91eff
Stefan Philipp <stefan.philipp@secunet.com>
2025-08-15 12:21:13 +0200

    PS-27: Fix problem "Interface change via menu only works with interface tab in foreground"

--- de.persosim.driver.connector.ui/src/de/persosim/driver/connector/ui/handlers/StandardReaderHandler.java
+++ de.persosim.driver.connector.ui/src/de/persosim/driver/connector/ui/handlers/StandardReaderHandler.java
@@ -4,18 +4,29 @@ import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.menu.MItem;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
-import de.persosim.driver.connector.service.IfdConnectorImpl;
 import de.persosim.driver.connector.ui.parts.ReaderPart;
 import de.persosim.driver.connector.ui.parts.ReaderPart.ReaderType;
+import jakarta.inject.Inject;
 
-public class StandardReaderHandler {
-	IfdConnectorImpl connector;
-	
+public class StandardReaderHandler
+{
+	private EPartService partService;
+
+	@Inject
+	public StandardReaderHandler(EPartService partService)
+	{
+		this.partService = partService;
+	}
+
 	@CanExecute
-	public boolean checkState(final MPart mPart, final MItem mItem) {
-		if (mPart.getObject() instanceof ReaderPart) {
-			ReaderPart readerPartObject = (ReaderPart) mPart.getObject();
+	public boolean checkState(final MItem mItem)
+	{
+		// ID of part as defined in fragment.e4xmi application model
+		MPart readerPart = partService.findPart("de.persosim.driver.connector.ui.parts.reader");
+		if (readerPart.getObject() instanceof ReaderPart mPart) {
+			ReaderPart readerPartObject = mPart;
 			mItem.setSelected(readerPartObject.getCurrentReaderType() == ReaderType.STANDARD);
 			return readerPartObject.getCurrentReaderType() != ReaderType.EXTERNAL;
 		}
@@ -23,11 +34,14 @@ public class StandardReaderHandler {
 	}
 
 	@Execute
-	public void execute(final MPart mPart, final MItem mItem) {
-		if (mPart.getObject() instanceof ReaderPart) {
-			ReaderPart readerPartObject = (ReaderPart) mPart.getObject();
-
+	public void execute()
+	{
+		// ID of part as defined in fragment.e4xmi application model
+		MPart readerPart = partService.findPart("de.persosim.driver.connector.ui.parts.reader");
+		if (readerPart.getObject() instanceof ReaderPart mPart) {
+			ReaderPart readerPartObject = mPart;
 			readerPartObject.switchReaderType(ReaderType.STANDARD);
 		}
 	}
+
 }
